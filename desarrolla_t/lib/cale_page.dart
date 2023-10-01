cbfd5b91d5c051bf3d453273f0eaa43c05270e53
NellyGS01 <100099928+NellyGS01@users.noreply.github.com>
2023-09-30 21:46:32 -0600

    Cambios menores

--- desarrolla_t/lib/cale_page.dart
+++ desarrolla_t/lib/cale_page.dart
@@ -18,78 +18,80 @@ class _TableBasicsExampleState extends State<TableBasicsExample> {
       appBar: AppBar(
         title: Text('TableCalendar - Basics'),
       ),
-      body: Column(
-        children: [
-          TableCalendar(
-            firstDay: DateTime.utc(2010, 10, 16),
-            lastDay: DateTime.utc(2030, 3, 14),
-            focusedDay: _focusedDay,
-            calendarFormat: _calendarFormat,
-            selectedDayPredicate: (day) {
-              // Use `selectedDayPredicate` to determine which day is currently selected.
-              // If this returns true, then `day` will be marked as selected.
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            TableCalendar(
+              firstDay: DateTime.utc(2010, 10, 16),
+              lastDay: DateTime.utc(2030, 3, 14),
+              focusedDay: _focusedDay,
+              calendarFormat: _calendarFormat,
+              selectedDayPredicate: (day) {
+                // Use `selectedDayPredicate` to determine which day is currently selected.
+                // If this returns true, then `day` will be marked as selected.
 
-              // Using `isSameDay` is recommended to disregard
-              // the time-part of compared DateTime objects.
-              return isSameDay(_selectedDay, day);
-            },
-            onDaySelected: (selectedDay, focusedDay) {
-              if (!isSameDay(_selectedDay, selectedDay)) {
-                // Call `setState()` when updating the selected day
-                setState(() {
-                  _selectedDay = selectedDay;
-                  _focusedDay = focusedDay;
-                });
-              }
-            },
-            onFormatChanged: (format) {
-              if (_calendarFormat != format) {
-                // Call `setState()` when updating calendar format
-                setState(() {
-                  _calendarFormat = format;
-                });
-              }
-            },
-            onPageChanged: (focusedDay) {
-              // No need to call `setState()` here
-              _focusedDay = focusedDay;
-            },
-          ),
-          const Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: EdgeInsetsDirectional.fromSTEB(0, 10, 20, 0),
-                child: Text(
-                  'Hoy',
-                  style: TextStyle(fontSize: 30),
+                // Using `isSameDay` is recommended to disregard
+                // the time-part of compared DateTime objects.
+                return isSameDay(_selectedDay, day);
+              },
+              onDaySelected: (selectedDay, focusedDay) {
+                if (!isSameDay(_selectedDay, selectedDay)) {
+                  // Call `setState()` when updating the selected day
+                  setState(() {
+                    _selectedDay = selectedDay;
+                    _focusedDay = focusedDay;
+                  });
+                }
+              },
+              onFormatChanged: (format) {
+                if (_calendarFormat != format) {
+                  // Call `setState()` when updating calendar format
+                  setState(() {
+                    _calendarFormat = format;
+                  });
+                }
+              },
+              onPageChanged: (focusedDay) {
+                // No need to call `setState()` here
+                _focusedDay = focusedDay;
+              },
+            ),
+            const Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(0, 10, 20, 0),
+                  child: Text(
+                    'Hoy',
+                    style: TextStyle(fontSize: 30),
+                  ),
                 ),
-              ),
-              SizedBox(height: 10),
-              InfoPendiente(),
-              SizedBox(height: 10),
-              InfoPendiente()
-            ],
-          ),
-          const Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: EdgeInsetsDirectional.fromSTEB(0, 10, 20, 0),
-                child: Text(
-                  'Pendientes',
-                  style: TextStyle(fontSize: 30),
+                SizedBox(height: 10),
+                InfoPendiente(),
+                SizedBox(height: 10),
+                InfoPendiente()
+              ],
+            ),
+            const Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(0, 10, 20, 0),
+                  child: Text(
+                    'Pendientes',
+                    style: TextStyle(fontSize: 30),
+                  ),
                 ),
-              ),
-              SizedBox(height: 10),
-              InfoPendientes(),
-              SizedBox(height: 10),
-              InfoPendientes()
-            ],
-          ),
-        ],
+                SizedBox(height: 10),
+                InfoPendientes(),
+                SizedBox(height: 10),
+                InfoPendientes()
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
