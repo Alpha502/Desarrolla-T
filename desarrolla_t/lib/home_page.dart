3ae07d7b04aaca969d37e1c738aa14d179835379
Oscar Hernández <ohernandez@grupoluthe.com>
2023-11-01 01:49:48 -0600

    Modificaciones tema
    no quiere refrescar el tema correctamente

--- desarrolla_t/lib/home_page.dart
+++ desarrolla_t/lib/home_page.dart
@@ -1,101 +1,127 @@
+import 'package:desarrolla_t/theme_model.dart';
 import 'package:flutter/material.dart';
 import 'package:desarrolla_t/widget_page.dart';
 import 'package:desarrolla_t/newEvent_page.dart';
+import 'package:provider/provider.dart';
 
 //PAGINA PRINCIPAL
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   final List<String> items;
 
   const HomePage({super.key, required this.items});
 
   @override
+  State<HomePage> createState() => _HomePage(items);
+  
+}
+
+class _HomePage extends State<HomePage>{
+
+  final List<String> items;
+
+  _HomePage(this.items);
+
+  @override
   Widget build(BuildContext context) {
     const title = 'Desarrolla-T';
 
-    return MaterialApp(
-        title: title,
-        home: Scaffold(
-            drawer: const Drawer(child: Costado()),
-            appBar: AppBar(
-              title: const Text(title),
-              actions: <Widget>[
-                Builder(builder: (BuildContext context) {
-                  return IconButton(
-                    icon: Icon(Icons.add),
-                    selectedIcon: const Icon(Icons.add),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const NewEvent()),
+    return Consumer(builder: (context, ThemeModel themeNotifier, child){
+      return MaterialApp(
+            title: title,
+            home: Scaffold(
+                drawer: const Drawer(child: Costado()),
+                appBar: AppBar(
+                  title: const Text(title),
+                  actions: <Widget>[
+                    Builder(builder: (BuildContext context) {
+                      return IconButton(
+                        icon: Icon(Icons.add),
+                        selectedIcon: const Icon(Icons.add),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const NewEvent()),
+                          );
+                        },
                       );
-                    },
-                  );
-                })
-              ],
-            ),
-            body: SingleChildScrollView(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Image.network(
-                      'https://docs.flutter.dev/assets/images/dash/dash-fainting.gif'),
-                  const Padding(
-                    padding: EdgeInsetsDirectional.fromSTEB(0, 10, 20, 25),
-                    child: Text(
-                      'Agenda Del dia',
-                      style: TextStyle(fontSize: 30),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 150,
-                    child: ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemCount: items.length,
-                      itemBuilder: (context, index) {
-                        return const Center(
-                          child: Row(
-                            children: [
-                              SizedBox(
-                                width: 12,
+                    }),
+                    IconButton(
+                        icon: Icon(themeNotifier.isDark ? Icons.wb_sunny : Icons.nightlight_round),
+                        selectedIcon: const Icon(Icons.add),
+                        onPressed: () {
+                          themeNotifier.isDark ? themeNotifier.isDark = false
+                          : themeNotifier.isDark = true;
+                        },
+                      ),
+                  ],
+                  //backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+                ),
+                body: SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Image.network(
+                          'https://static.vecteezy.com/system/resources/previews/005/892/011/original/back-to-school-banner-with-hand-drawn-line-art-icons-of-education-science-objects-and-office-supplies-school-supplies-concept-of-education-background-free-vector.jpg'),
+                      const Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(0, 10, 20, 25),
+                        child: Text(
+                          'Agenda Del dia',
+                          style: TextStyle(fontSize: 30),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 150,
+                        child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: items.length,
+                          itemBuilder: (context, index) {
+                            return const Center(
+                              child: Row(
+                                children: [
+                                  SizedBox(
+                                    width: 12,
+                                  ),
+                                  InfoTarjeta(),
+                                ],
                               ),
-                              InfoTarjeta(),
-                            ],
-                          ),
-                        );
-                      },
-                    ),
-                  ),
-                  const Padding(
-                    padding: EdgeInsetsDirectional.fromSTEB(0, 10, 20, 25),
-                    child: Text(
-                      'Pendientes',
-                      style: TextStyle(fontSize: 30),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 500,
-                    child: ListView.builder(
-                      scrollDirection: Axis.vertical,
-                      itemCount: items.length,
-                      itemBuilder: (context, index) {
-                        return const Center(
-                          child: Column(
-                            children: [
-                              SizedBox(
-                                height: 12,
+                            );
+                          },
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(0, 10, 20, 25),
+                        child: Text(
+                          'Pendientes',
+                          style: TextStyle(fontSize: 30),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 500,
+                        child: ListView.builder(
+                          scrollDirection: Axis.vertical,
+                          itemCount: items.length,
+                          itemBuilder: (context, index) {
+                            return const Center(
+                              child: Column(
+                                children: [
+                                  SizedBox(
+                                    height: 12,
+                                  ),
+                                  InfoPendiente()
+                                ],
                               ),
-                              InfoPendiente()
-                            ],
-                          ),
-                        );
-                      },
-                    ),
+                            );
+                          },
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-            )));
+                )
+              )
+          );
+    });
   }
 }
--- desarrolla_t/lib/main.dart
+++ desarrolla_t/lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:desarrolla_t/habits_page.dart';
 import 'package:desarrolla_t/login_page.dart';
+import 'package:desarrolla_t/theme_model.dart';
 import 'package:flutter/material.dart';
 import 'package:desarrolla_t/home_page.dart';
 import 'package:desarrolla_t/cale_page.dart';
 import 'package:desarrolla_t/newEvent_page.dart';
 import 'package:desarrolla_t/info_page.dart';
 import 'package:desarrolla_t/editEvent_page.dart';
+import 'package:provider/provider.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+/*
 void main() => runApp(MaterialApp(
       title: 'Rutas',
       home: const LoginPage(),
@@ -21,3 +27,31 @@ void main() => runApp(MaterialApp(
             HomePage(items: List<String>.generate(5, (i) => 'Item $i')),
       },
     ));
+*/
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => ThemeModel(),
+      child: Consumer(builder: (context, ThemeModel themeNotifier, child){
+        return MaterialApp(
+          title: 'Rutas',
+          theme: themeNotifier.isDark ? ThemeData.dark() : ThemeData.light(),
+          home: const LoginPage(),
+          routes: {
+            '/habitpage': (context) => const Habits(),
+            '/newpage': (context) => const NewEvent(),
+            '/infopage': (context) => const InfoPage(),
+            '/editeventpage': (context) => const ModifyEvent(),
+            '/calpage': (context) => const TableBasicsExample(),
+            '/loginpage' : (context) => const LoginPage(),
+            '/homepage': (context) => HomePage(items: List<String>.generate(5, (i) => 'Item $i')),
+          },
+        );
+      },),
+    );
+  }
+}
